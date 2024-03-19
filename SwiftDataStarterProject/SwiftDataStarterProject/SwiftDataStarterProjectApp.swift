f8dbdf81f75c4d6c53f5395b27589b73527d39be
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-03-19 05:52:35 +0100

    Initial commit Swift Data Starter Project

---
+++ SwiftDataStarterProject/SwiftDataStarterProject/SwiftDataStarterProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataStarterProjectApp.swift
+//  SwiftDataStarterProject
+//
+//  Created by HubertMac on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataStarterProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
