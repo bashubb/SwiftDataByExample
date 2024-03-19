87912dfb808db4f1145e1340c4a07fbdfee1d811
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-03-19 10:00:47 +0100

    Filtering the results from a Swift Data Query

--- SwiftDataStarterProject/SwiftDataStarterProject/ContentView.swift
+++ SwiftDataStarterProject/SwiftDataStarterProject/ContentView.swift
@@ -12,12 +12,14 @@ struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @State private var path = [Destination]()
     @State private var sortOrder = SortDescriptor(\Destination.name)
+    @State private var searchText = ""
     
     var body: some View {
         NavigationStack(path: $path) {
-            DestinationListingView(sort: sortOrder)
+            DestinationListingView(sort: sortOrder, searchString: searchText)
+                .searchable(text: $searchText)
                 .navigationTitle("iTour")
-                .navigationDestination(for: Destination.self) { destination in
+                .navigationDestination(for: Destination.self){ destination in
                     EditDestinationView(destination: destination)
                 }
                 .toolbar{
--- SwiftDataStarterProject/SwiftDataStarterProject/DestinationListingView.swift
+++ SwiftDataStarterProject/SwiftDataStarterProject/DestinationListingView.swift
@@ -35,11 +35,17 @@ struct DestinationListingView: View {
         }
     }
     
-    init(sort: SortDescriptor<Destination>) {
-        _destinations = Query(sort: [sort])
+    init(sort: SortDescriptor<Destination>, searchString: String) {
+        _destinations = Query(filter: #Predicate {
+            if searchString.isEmpty {
+                return true
+            } else {
+                return $0.name.localizedStandardContains(searchString)
+            }
+        },sort: [sort])
     }
 }
 
 #Preview {
-    DestinationListingView(sort: SortDescriptor(\Destination.name))
+    DestinationListingView(sort: SortDescriptor(\Destination.name), searchString: "")
 }
